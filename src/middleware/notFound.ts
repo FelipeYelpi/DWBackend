45a09687b40ac0f7a58d4ed258d1dbf5f4367b74
FelipeYelpi <felipe.yelpi@gmail.com>
2025-11-03 23:50:33 -0300

    Update notFound.ts

--- src/middleware/notFound.ts
+++ src/middleware/notFound.ts
@@ -1,1 +1,6 @@
 
+import { Request, Response, NextFunction } from "express";
+
+export function notFound(req: Request, res: Response, next: NextFunction) {
+  res.status(404).json({ error: "Not found" });
+}
